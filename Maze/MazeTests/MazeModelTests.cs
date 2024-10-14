463c8caf23461cacb44c5041bdad9cf67ecef293
darrpama <darrpama@darrpamas-MacBook-Air.local>
2024-10-14 11:03:42 +0300

    test is not passed

--- Maze/MazeTests/MazeModelTests.cs
+++ Maze/MazeTests/MazeModelTests.cs
@@ -11,24 +11,40 @@ public class MazeModelTests
     {
         var numbers = new[]
         {
-            0, 1, 0, 0, 1, 1, 0, 1, 0, 0,
-            0, 0, 1, 1, 0, 1, 0, 1, 1, 0,
-            1, 0, 1, 1, 0, 0, 0, 0, 0, 1,
-            0, 1, 1, 0, 0, 1, 0, 1, 1, 0,
-            0, 0, 1, 0, 1, 1, 1, 0
+            0, 0, 0, 1,
+            1, 0, 1, 1,
+            0, 1, 0, 1,
+            0, 0, 0, 1,
+            1, 0, 1, 0,
+            0, 0, 1, 0,
+            1, 1, 0, 1,
+            1, 1, 1, 1
         };
+        
         IGenerator generator = new SequenceGenerator(numbers);
         var mazeGenerator = new MazeGenerator(generator);
         var result = mazeGenerator.Generate(4, 4);
 
-        // var expected = new MazePoint[,]
-        // {
-        //     {new(false,false,0,0),new(true,true,0,1),  new(true,false,0,2), new(false,true,0,3)},
-        //     {new(false,true,1,0), new(false,false,1,1),new(true,false,1,2 ),new(true,true,1,3)},
-        //     {new(true,false,2,0), new(false,true,2,1), new(false,false,2,2),new(true,true,2,3)},
-        //     {new(true,false,3,0), new(true,false,3,1 ),new(true,false,3,2), new(true,true,3,3)},
-        // };
+        var expected = new MazePoint[,]
+        {
+            {new(true,false,0,0),new(false,false,0,1),  new(true,false,0,2), new(false,true,0,3)},
+            {new(false,true,1,0), new(false,false,1,1),new(true,true,1,2 ),new(false,true,1,3)},
+            {new(true,false,2,0), new(true,true,2,1), new(false,false,2,2),new(true,true,2,3)},
+            {new(true,false,3,0), new(true,false,3,1 ),new(true,false,3,2), new(true,true,3,3)},
+        };
+        
         Assert.Equal(4, result.GetLength(0));
         Assert.Equal(4, result.GetLength(1));
+        Assert.Equal(expected, result);
+        
+    //     for (var i = 0; i < result.GetLength(0); i++)
+    //     {
+    //         for (var j = 0; j < result.GetLength(1); j++)
+    //         {
+    //             Console.Write(result[i, j].F);
+    //             Console.Write(" ");
+    //         }
+    //         Console.WriteLine();
+    //     }
     }
 }
